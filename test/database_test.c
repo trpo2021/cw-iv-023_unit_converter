e98c035c9220035c340df921748a40b01fd3f779
Dmitry Stacenko <dimastacenko104@gmail.com>
2021-05-05 12:33:29 +0700

    Implement test for line_counter function

---
+++ test/database_test.c
@@ -0,0 +1,18 @@
+#include "ctest.h"
+#include "lib_unit_converter/unit_converter.h"
+
+CTEST(line_counter, no_empty_file)
+{
+    int expect = 3;
+    FILE* file = fopen("test/files_for_tests/line_count_test.txt", "r");
+    int real = line_counter(file);
+    ASSERT_EQUAL(expect, real);
+}
+
+CTEST(line_counter, empty_file)
+{
+    int expect = 0;
+    FILE* file = fopen("test/files_for_tests/empty_file.txt", "r");
+    int real = line_counter(file);
+    ASSERT_EQUAL(expect, real);
+}
