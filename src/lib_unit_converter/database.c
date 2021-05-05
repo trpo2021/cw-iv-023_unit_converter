42bad6b2805abf8e4938aea1031b267655cc2373
Dmitry Stacenko <dimastacenko104@gmail.com>
2021-05-05 18:20:30 +0700

    Implement database_create function

--- src/lib_unit_converter/database.c
+++ src/lib_unit_converter/database.c
@@ -81,6 +81,23 @@ category* get_category(FILE* file)
     return arr_categors;
 }
 
+category** database_create(FILE* file)
+{
+    int counter_line = line_counter(file);
+    if (counter_line == 0) {
+        printf("EROR file empty\n");
+        return NULL;
+    }
+    category** arr_categors = malloc(sizeof(category) * counter_line);
+    for (int i = 0; i < counter_line; i++) {
+        arr_categors[i] = get_category(file);
+        if (arr_categors[i] == NULL) {
+            printf("EROR: Category format №%d is not correct\n", (i + 1));
+        }
+    }
+    return arr_categors;
+}
+
 int line_counter(FILE* file)
 {
     int counter = 0;
