bce0afdbfb8c6ed4967584ee159974f398543e82
Nikolay Kavirshin <kolyancik02@gmail.com>
2021-05-08 14:53:04 +0700

    rename time_convert to convert_time

--- src/lib_unit_converter/time_converter.c
+++ src/lib_unit_converter/time_converter.c
@@ -1,6 +1,6 @@
 #include "lib_unit_converter/unit_converter.h"
 
-double time_convert(double factor_from, double factor_in, double value)
+double convert_time(double factor_from, double factor_in, double value)
 {
     return value * (factor_from / factor_in);
 }
