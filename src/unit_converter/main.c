64009855f6362334c8654ce1b0d40b354752f78e
Dmitry Stacenko <dimastacenko104@gmail.com>
2021-05-08 15:53:13 +0700

    Modify main add new variables

--- src/unit_converter/main.c
+++ src/unit_converter/main.c
@@ -20,11 +20,12 @@ int main()
     input_str = calloc(SIZE_INPUT_STR, sizeof(char));
     fgets(input_str, SIZE_INPUT_STR, stdin);
 
+    int first_factor = 1, second_factor = 2;
     int index = get_index_cat(input_str, arr_categors, counter_line);
-    double factor_from = get_factor(input_str, index, 1, arr_categors);
+    double factor_from = get_factor(input_str, index, first_factor, arr_categors);
     printf("factor_from = %.10lf\n", factor_from);
     // сделано для проверки и для того чтобы main компилировался, позже удалим.
-    double factor_in = get_factor(input_str, index, 2, arr_categors);
+    double factor_in = get_factor(input_str, index, second_factor, arr_categors);
     printf("factor_in = %.10lf\n", factor_in);
     // сделано для проверки и для того чтобы main компилировался, позже удалим.
     double value = get_value(input_str);
