8035ea7d55700439e0f4a19aa2b90dec37073616
Dmitry Stacenko <dimastacenko104@gmail.com>
2021-05-08 23:26:26 +0700

    Fix code style

--- src/unit_converter/main.c
+++ src/unit_converter/main.c
@@ -22,10 +22,12 @@ int main()
 
     int first_factor = 1, second_factor = 2;
     int index = get_index_cat(input_str, arr_categors, counter_line);
-    double factor_from = get_factor(input_str, index, first_factor, arr_categors);
+    double factor_from;
+    factor_from = get_factor(input_str, index, first_factor, arr_categors);
     printf("factor_from = %.10lf\n", factor_from);
     // сделано для проверки и для того чтобы main компилировался, позже удалим.
-    double factor_in = get_factor(input_str, index, second_factor, arr_categors);
+    double factor_in;
+    factor_from = get_factor(input_str, index, second_factor, arr_categors);
     printf("factor_in = %.10lf\n", factor_in);
     // сделано для проверки и для того чтобы main компилировался, позже удалим.
     double value = get_value(input_str);
