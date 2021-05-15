739f830aa5c35345c341c4f4503bfdbb6b510741
Nikolay Kavirshin <kolyancik02@gmail.com>
2021-05-15 14:35:20 +0700

    fixed repeating the same code

--- src/lib_unit_converter/unit_converter.c
+++ src/lib_unit_converter/unit_converter.c
@@ -1,24 +1,14 @@
 #include "lib_unit_converter/unit_converter.h"
 
-double convert_length(double factor_from, double factor_in, double value)
+double convert_unit(double factor_from, double factor_in, double value)
 {
     return value * (factor_from / factor_in);
 }
 
-double convert_speed(double factor_from, double factor_in, double value)
+double convert_unit_speed(double factor_from, double factor_in, double value)
 {
     double converted_value;
     converted_value = value * (1 / factor_from) * factor_in;
 
     return converted_value;
-}
-
-double convert_weight(double factor_from, double factor_in, double value)
-{
-    return value * (factor_from / factor_in);
-}
-
-double convert_time(double factor_from, double factor_in, double value)
-{
-    return value * (factor_from / factor_in);
 }
