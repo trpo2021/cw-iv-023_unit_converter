08ee17fda2e9d2f7cb3862b194e5a255669605bd
Nikolay Kavirshin <kolyancik02@gmail.com>
2021-05-23 17:03:47 +0700

    Create request input

--- src/unit_converter/main.c
+++ src/unit_converter/main.c
@@ -1,11 +1,11 @@
 #include "lib_unit_converter/unit_converter.h"
 
-int main()
+int main(int argc, char** argv)
 {
     FILE* file;
     file = fopen("src/unit_converter/data.txt", "r");
     if (file == NULL) {
-        printf("EROR cant open file\n");
+        printf("ERROR cant open file\n");
         return 1;
     }
     int counter_line = line_counter(file);
@@ -17,9 +17,13 @@ int main()
     fclose(file);
 
     char input_str[SIZE_INPUT_STR] = {0};
+    printf("You want:");
     fgets(input_str, SIZE_INPUT_STR, stdin);
     printf("converted value = %lf\n",
            convert(input_str, arr_categors, counter_line));
     free_database(arr_categors, counter_line);
+    for(int i = 1; i < 3; i++){
+    printf("%d   %s\n", argc, argv[i]);
+    }
     return 0;
 }
