38e2a54689fc489fa1f15d8e94e016189217478e
Dmitry Stacenko <dimastacenko104@gmail.com>
2021-05-24 01:02:34 +0700

    Modify main to check input data for errors

--- src/unit_converter/main.c
+++ src/unit_converter/main.c
@@ -1,6 +1,6 @@
 #include "lib_unit_converter/unit_converter.h"
 
-int main(int argc, char** argv)
+int main()
 {
     FILE* file;
     file = fopen("src/unit_converter/data.txt", "r");
@@ -17,14 +17,21 @@ int main(int argc, char** argv)
     fclose(file);
 
     char input_str[SIZE_INPUT_STR] = {0};
-    if ((strcmp(argv[1], "--fast") == 0) || (strcmp(argv[1], "-f") == 0)) {
-        build_str_fast(input_str, argc, argv);
-    } else {
-        printf("You want:");
-        fgets(input_str, SIZE_INPUT_STR, stdin);
+    fgets(input_str, SIZE_INPUT_STR, stdin);
+    char* correct_str = NULL;
+    int code_error = correct_input_str(input_str, &correct_str);
+    if (code_error < 0) {
+        print_errors(code_error);
+        free_database(arr_categors, counter_line);
+        return 1;
     }
-    printf("Converted value = %lf\n",
-           convert(input_str, arr_categors, counter_line));
+    double converted_value = convert(correct_str, arr_categors, counter_line);
+    if (converted_value < 0) {
+        print_errors(converted_value);
+        free_database(arr_categors, counter_line);
+        return 1;
+    }
+    printf("Converted value = %lf\n", converted_value);
     free_database(arr_categors, counter_line);
     return 0;
 }
