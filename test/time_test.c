cfb6725d17438e14e6ab7a63158d087c9307214e
Dmitry Stacenko <dimastacenko104@gmail.com>
2021-05-23 20:29:51 +0700

    Change input data according to the new template

--- test/time_test.c
+++ test/time_test.c
@@ -6,7 +6,7 @@ CTEST(time_test, s_in_ms)
     FILE* file = fopen("test/files_for_tests/data_time_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Time(s, ms, 1)";
+    char input_str[] = "Time(s ms, 1)";
     double expect = 1000;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -16,7 +16,7 @@ CTEST(time_test, ms_in_s)
     FILE* file = fopen("test/files_for_tests/data_time_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Time(ms, s, 1)";
+    char input_str[] = "Time(ms s, 1)";
     double expect = 0.001;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -26,7 +26,7 @@ CTEST(time_test, ns_in_s)
     FILE* file = fopen("test/files_for_tests/data_time_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Time(ns, s, 36000)";
+    char input_str[] = "Time(ns s, 36000)";
     double expect = 3.6e-5;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -36,7 +36,7 @@ CTEST(time_test, h_in_ns)
     FILE* file = fopen("test/files_for_tests/data_time_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Time(h, ns, 1)";
+    char input_str[] = "Time(h ns, 1)";
     double expect = 3.6e+12;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -46,7 +46,7 @@ CTEST(time_test, w_in_mks)
     FILE* file = fopen("test/files_for_tests/data_time_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Time(w, mks, 1)";
+    char input_str[] = "Time(w mks, 1)";
     double expect = 6.048e+11;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -56,7 +56,7 @@ CTEST(time_test, mks_in_w)
     FILE* file = fopen("test/files_for_tests/data_time_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Time(mks, w, 6048)";
+    char input_str[] = "Time(mks w, 6048)";
     double expect = 1e-8;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -66,7 +66,7 @@ CTEST(time_test, m_in_ns)
     FILE* file = fopen("test/files_for_tests/data_time_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Time(m, ns, 1)";
+    char input_str[] = "Time(m ns, 1)";
     double expect = 6e+10;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -76,7 +76,7 @@ CTEST(time_test, ns_in_m)
     FILE* file = fopen("test/files_for_tests/data_time_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Time(ns, m, 6)";
+    char input_str[] = "Time(ns m, 6)";
     double expect = 1e-10;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -86,7 +86,7 @@ CTEST(time_test, d_in_w)
     FILE* file = fopen("test/files_for_tests/data_time_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Time(d, w, 7)";
+    char input_str[] = "Time(d w, 7)";
     double expect = 1;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -96,7 +96,7 @@ CTEST(time_test, w_in_d)
     FILE* file = fopen("test/files_for_tests/data_time_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Time(w, d, 1)";
+    char input_str[] = "Time(w d, 1)";
     double expect = 7;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -106,7 +106,7 @@ CTEST(time_test, ms_in_m)
     FILE* file = fopen("test/files_for_tests/data_time_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Time(ms, m, 6)";
+    char input_str[] = "Time(ms m, 6)";
     double expect = 1e-4;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -116,7 +116,7 @@ CTEST(time_test, m_in_ms)
     FILE* file = fopen("test/files_for_tests/data_time_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Time(m, ms, 1)";
+    char input_str[] = "Time(m ms, 1)";
     double expect = 60000;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
--- test/weight_test.c
+++ test/weight_test.c
@@ -6,7 +6,7 @@ CTEST(convert_weight, g_in_kg)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(g, kg, 10)";
+    char input_str[] = "Weight(g kg, 10)";
     double expect = 0.01;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -16,7 +16,7 @@ CTEST(convert_weight, g_in_mkg)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(g, mkg, 10)";
+    char input_str[] = "Weight(g mkg, 10)";
     double expect = 1e+7;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -26,7 +26,7 @@ CTEST(convert_weight, g_in_mg)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(g, mg, 10)";
+    char input_str[] = "Weight(g mg, 10)";
     double expect = 10000;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -36,7 +36,7 @@ CTEST(convert_weight, g_in_q)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(g, q, 10)";
+    char input_str[] = "Weight(g q, 10)";
     double expect = 0.0001;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -46,7 +46,7 @@ CTEST(convert_weight, g_in_t)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(g, t, 10)";
+    char input_str[] = "Weight(g t, 10)";
     double expect = 0.00001;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -56,7 +56,7 @@ CTEST(convert_weight, kg_in_mkg)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(kg, mkg, 1)";
+    char input_str[] = "Weight(kg mkg, 1)";
     double expect = 1000000000;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -66,7 +66,7 @@ CTEST(convert_weight, kg_in_mg)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(kg, mg, 10)";
+    char input_str[] = "Weight(kg mg, 10)";
     double expect = 10000000;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -76,7 +76,7 @@ CTEST(convert_weight, kg_in_g)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(kg, g, 10)";
+    char input_str[] = "Weight(kg g, 10)";
     double expect = 10000;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -86,7 +86,7 @@ CTEST(convert_weight, kg_in_q)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(kg, q, 10)";
+    char input_str[] = "Weight(kg q, 10)";
     double expect = 0.1;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -96,7 +96,7 @@ CTEST(convert_weight, kg_in_t)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(kg, t, 10)";
+    char input_str[] = "Weight(kg t, 10)";
     double expect = 0.01;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -106,7 +106,7 @@ CTEST(convert_weight, mkg_in_mg)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(mkg, mg, 10)";
+    char input_str[] = "Weight(mkg mg, 10)";
     double expect = 0.01;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -116,7 +116,7 @@ CTEST(convert_weight, mkg_in_g)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(mkg, g, 10)";
+    char input_str[] = "Weight(mkg g, 10)";
     double expect = 0.00001;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -126,7 +126,7 @@ CTEST(convert_weight, mkg_in_kg)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(mkg, kg, 10)";
+    char input_str[] = "Weight(mkg kg, 10)";
     double expect = 0.00000001;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -136,7 +136,7 @@ CTEST(convert_weight, mkg_in_q)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(mkg, q, 10)";
+    char input_str[] = "Weight(mkg q, 10)";
     double expect = 0.0000000001;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -146,7 +146,7 @@ CTEST(convert_weight, mkg_in_t)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(mkg, t, 10)";
+    char input_str[] = "Weight(mkg t, 10)";
     double expect = 0.00000000001;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -156,7 +156,7 @@ CTEST(convert_weight, mg_in_mkg)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(mg, mkg, 10)";
+    char input_str[] = "Weight(mg mkg, 10)";
     double expect = 10000;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -166,7 +166,7 @@ CTEST(convert_weight, mg_in_g)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(mg, g, 10)";
+    char input_str[] = "Weight(mg g, 10)";
     double expect = 0.01;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -176,7 +176,7 @@ CTEST(convert_weight, mg_in_kg)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(mg, kg, 10)";
+    char input_str[] = "Weight(mg kg, 10)";
     double expect = 0.00001;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -186,7 +186,7 @@ CTEST(convert_weight, mg_in_q)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(mg, q, 10)";
+    char input_str[] = "Weight(mg q, 10)";
     double expect = 0.0000001;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -196,7 +196,7 @@ CTEST(convert_weight, mg_in_t)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(mg, t, 10)";
+    char input_str[] = "Weight(mg t, 10)";
     double expect = 0.00000001;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -206,7 +206,7 @@ CTEST(convert_weight, q_in_mkg)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(q, mkg, 10)";
+    char input_str[] = "Weight(q mkg, 10)";
     double expect = 1000000000000;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -216,7 +216,7 @@ CTEST(convert_weight, q_in_mg)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(q, mg, 10)";
+    char input_str[] = "Weight(q mg, 10)";
     double expect = 1000000000;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -226,7 +226,7 @@ CTEST(convert_weight, q_in_g)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(q, g, 10)";
+    char input_str[] = "Weight(q g, 10)";
     double expect = 1000000;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -236,7 +236,7 @@ CTEST(convert_weight, q_in_kg)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(q, kg, 10)";
+    char input_str[] = "Weight(q kg, 10)";
     double expect = 1000;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -246,7 +246,7 @@ CTEST(convert_weight, q_in_t)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(q, t, 10)";
+    char input_str[] = "Weight(q t, 10)";
     double expect = 1;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -256,7 +256,7 @@ CTEST(convert_weight, t_in_mkg)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(t, mkg, 0.0001)";
+    char input_str[] = "Weight(t mkg, 0.0001)";
     double expect = 100000000;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -266,7 +266,7 @@ CTEST(convert_weight, t_in_mg)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(t, mg, 10)";
+    char input_str[] = "Weight(t mg, 10)";
     double expect = 10000000000;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -276,7 +276,7 @@ CTEST(convert_weight, t_in_g)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(t, g, 10)";
+    char input_str[] = "Weight(t g, 10)";
     double expect = 10000000;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -286,7 +286,7 @@ CTEST(convert_weight, t_in_kg)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(t, kg, 10)";
+    char input_str[] = "Weight(t kg, 10)";
     double expect = 10000;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
@@ -296,7 +296,7 @@ CTEST(convert_weight, t_in_q)
     FILE* file = fopen("test/files_for_tests/data_weight_test.txt", "r");
     category* ar_cat = database_create(file, 1);
     fclose(file);
-    char input_str[] = "Weight(t, q, 10)";
+    char input_str[] = "Weight(t q, 10)";
     double expect = 100;
     ASSERT_DBL_NEAR_TOL(expect, convert(input_str, ar_cat, 1), TOLL);
 }
