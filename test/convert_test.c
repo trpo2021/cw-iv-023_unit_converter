3b9094326963f4660eb0b82785eda8430b0a52d2
BABKA <begor@ngs.ru>
2021-05-31 18:36:42 +0700

    Implement tests for frequecy

--- test/convert_test.c
+++ test/convert_test.c
@@ -113,6 +113,22 @@ CTEST(convert_test, datasize)
     fclose(file_test);
 }
 
+CTEST(convert_test, frequency)
+{
+    FILE* file_categories = fopen("src/unit_converter/database.txt", "r");
+    category* arr_cat = database_create(file_categories, 8);
+    fclose(file_categories);
+
+    FILE* file_test = fopen("test/files_for_tests/frequency_test.txt", "r");
+    test_data data;
+
+    while (!feof(file_test)) {
+        get_test_data(file_test, &data);
+        ASSERT_DBL_NEAR_TOL(data.expect, convert(data.input, arr_cat, 8), 1e-06);
+    }
+    fclose(file_test);
+}
+
 CTEST(convert_test, area)
 {
     FILE* file_categories = fopen("src/unit_converter/database.txt", "r");
